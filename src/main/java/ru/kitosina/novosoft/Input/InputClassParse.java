67f11482852ee5bd81b50376dcde8b468c7f5da2
kitosina <kitosina@mail.ru>
2020-09-08 19:19:40 +0700

    Implementation of parameter input

---
+++ src/main/java/ru/kitosina/novosoft/Input/InputClassParse.java
@@ -0,0 +1,51 @@
+package ru.kitosina.novosoft.Input;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class InputClassParse {
+
+    private Scanner sc = new Scanner(System.in);
+
+    private String path;
+    private Integer quantityClient;
+    private List<Integer> timeOutClient = new ArrayList<>();
+    private List<String> messageClient = new ArrayList<>();
+
+    public String inputPath() {
+        System.out.println("File path: ");
+        path = sc.nextLine();
+        return path;
+    }
+
+    public Integer inputQuantityClient() {
+        System.out.println("Quantity Client: ");
+        quantityClient = sc.nextInt();
+        return quantityClient;
+    }
+
+    public void addInputInfoClient() {
+        for (Integer i = 0; i< quantityClient; i++) {
+            timeOutClient.add(sc.nextInt());
+            messageClient.add(sc.next());
+        }
+    }
+
+    public List<Integer> getTimeOutClient() {
+        return timeOutClient;
+    }
+
+    public void setTimeOutClient(List<Integer> timeOutClient) {
+        this.timeOutClient = timeOutClient;
+    }
+
+    public List<String> getMessageClient() {
+        return messageClient;
+    }
+
+    public void setMessageClient(List<String> messageClient) {
+        this.messageClient = messageClient;
+    }
+}
--- src/main/java/ru/kitosina/novosoft/Main.java
+++ src/main/java/ru/kitosina/novosoft/Main.java
@@ -1,8 +1,16 @@
 package ru.kitosina.novosoft;
 
+import ru.kitosina.novosoft.Input.InputClassParse;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
+        InputClassParse inputClassParse = new InputClassParse();
+        String path = inputClassParse.inputPath();
+        Integer quantityClient = inputClassParse.inputQuantityClient();
+        inputClassParse.addInputInfoClient();
+        List<Integer> timeOutClient = inputClassParse.getTimeOutClient();
+        List<String> messageClient = inputClassParse.getMessageClient();
     }
 }
